c0b19d4946046dfbb63a55e1d21062fdb7e963d5
Hunter Herman <hunterlherman@gmail.com>
2018-02-05 20:03:22 -0800

    :/

--- Pods/Target Support Files/Pods-The Mac Weekly/Pods-The Mac Weekly-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_The_Mac_Weekly : NSObject
-@end
-@implementation PodsDummy_Pods_The_Mac_Weekly
-@end
--- Pods/Target Support Files/Pods-The Mac WeeklyTests/Pods-The Mac WeeklyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_The_Mac_WeeklyTests : NSObject
-@end
-@implementation PodsDummy_Pods_The_Mac_WeeklyTests
-@end
