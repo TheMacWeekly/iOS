72f99d2f0da6ac8131447f2fa214b3439659d0dd
Hunter Herman <hunterlherman@gmail.com>
2018-03-31 16:08:02 -0500

    Basic search

--- The Mac Weekly/InfiniteTableView.swift
+++ The Mac Weekly/InfiniteTableView.swift
@@ -12,43 +12,32 @@ import RxCocoa
 import Result
 import enum Siesta.ResourceEvent
 
+func selfDampener<T, U>(_ requests: Observable<T>, converter: @escaping (T) -> U) -> Observable<U.E>
+    where U: ObservableConvertibleType {
+        var awaitingResponse = false;
+        let dampenedRequests = requests.filter({_ in return !awaitingResponse}).do(onNext:{_ in awaitingResponse = true})
+        return dampenedRequests.flatMap(converter).do(onNext: {_ in awaitingResponse = false})
+}
 
 func pageFetcher<T>(_ requestTriggers: Observable<()>, fetchPage: @escaping (Int) -> Single<Result<T, AnyError>>) -> Observable<T> {
-    var page = 0
-    var loading = false
+    var page = 1
     
-    return
-        //Ignore request triggers if not loading
-        requestTriggers.filter({ !loading })
-            // when a request trigger passes, we are now loading
-            .do(onNext: { _ in
-                loading = true
-                
-            })
-            // map the request trigger to the result of the request (when that returns)
-            .flatMap({ fetchPage(page + 1) })
-            // when a request returns, we are no longer loading. If we succeeded
-            // we're on the next page
-            .do(onNext: { result in
-                loading = false
-                switch result {
-                case .success(_):
-                    page += 1
-                case _:
-                    break
-                }
-            })
-            // map request results to their data or nothing, to produce a pure
-            // data stream
-            .flatMap { result -> Observable<T> in
-                switch result {
-                case .success(let data):
-                    return Observable.just(data)
-                case _:
-                    return Observable.empty()
-                }
+    return selfDampener(requestTriggers, converter: { fetchPage(page) } )
+        .do(onNext: { result in
+            switch result {
+            case .success(_):
+                page += 1
+            case _:
+                break
             }
-    
+        }).flatMap({result -> Observable<T> in
+            switch result {
+            case .success(let data):
+                return Observable.just(data)
+            case _:
+                return Observable.empty()
+            }
+        })
     
 }
 
