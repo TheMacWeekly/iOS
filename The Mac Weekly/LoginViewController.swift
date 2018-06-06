8e0b5ec63db69f85ec7e684ef5fdf12f033d98f5
Gabe <gbrown3@macalester.edu>
2018-06-06 15:27:43 -0500

    Bare bones of a login page set up for testing

---
+++ The Mac Weekly/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  The Mac Weekly
+//
+//  Created by Gabriel Brown on 6/6/18.
+//  Copyright © 2018 The Mac Weekly. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+    @IBAction func login(_ sender: UIButton) {
+    }
+    
+    @IBAction func register(_ sender: UIButton) {
+    }
+    
+}
