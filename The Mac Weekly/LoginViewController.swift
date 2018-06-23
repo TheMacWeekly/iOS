350cea8f7a764c9f20dc033ee6b4f31039c0a1f5
Gabe <gbrown3@macalester.edu>
2018-06-22 18:02:05 -0700

    LoginViewController example now more explicit

--- The Mac Weekly/LoginViewController.swift
+++ The Mac Weekly/LoginViewController.swift
@@ -6,17 +6,32 @@
 //  Copyright © 2018 The Mac Weekly. All rights reserved.
 //
 
+// Note: this is just meant to serve as a rough example, mostly so
+// that you can see how to use the google sign in and other tools
+
 import UIKit
 import Firebase
 import GoogleSignIn
 
 class LoginViewController: UIViewController, GIDSignInUIDelegate {
     
+    @IBOutlet weak var emailEntry: UITextField!
+    @IBOutlet weak var passwordEntry: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // This is essential for our final login view controller!
         GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().signIn()
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        TestableUtils.login(email: emailEntry.text!, password: passwordEntry.text!)
+    }
+    
+    @IBAction func register(_ sender: UIButton) {
+        TestableUtils.register(email: emailEntry.text!, password: passwordEntry.text!)
     }
     
     @IBAction func SignOut(_ sender: UIButton) {
