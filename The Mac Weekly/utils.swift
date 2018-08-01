c28d29d11ca53d286a2a367709b3fdb1885c6969
Gabe <gbrown3@macalester.edu>
2018-08-01 16:02:33 -0700

    Moved UILabel extension to separate file
    It didn’t like it when I tried to include it with TestableUtils, and I
    don’t think it really fits there anyway.

--- The Mac Weekly/utils.swift
+++ The Mac Weekly/utils.swift
@@ -11,23 +11,6 @@ import Foundation
 import Result
 import Kingfisher
 
-
-// Source: https://stackoverflow.com/a/30711288
-extension UILabel {
-    func setHTMLFromString(htmlText: String) {
-        let modifiedFont = NSString(format:"<span style=\"font-family: '-apple-system', 'HelveticaNeue'; font-size: \(self.font!.pointSize)\">%@</span>" as NSString, htmlText) as String
-        
-        //process collection values
-        let attrStr = try! NSAttributedString(
-            data: modifiedFont.data(using: .unicode, allowLossyConversion: true)!,
-            options: [NSAttributedString.DocumentReadingOptionKey.documentType:NSAttributedString.DocumentType.html, NSAttributedString.DocumentReadingOptionKey.characterEncoding: String.Encoding.utf8.rawValue],
-            documentAttributes: nil)
-        
-        
-        self.attributedText = attrStr
-    }
-}
-
 // It doesn't seem like this function is being used. Should we just get rid of it?
 
 //func fixShadowImage(inView view: UIView) {
